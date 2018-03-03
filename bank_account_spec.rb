345b75e766c2b9343258d70bbe2916e938f9089f
Glynis Monroe <glynis.d.monroe@gmail.com>
2018-03-03 19:19:04 +0000

    this is my bank homework

---
+++ bank_account_spec.rb
@@ -0,0 +1,15 @@
+# TEST FILE bank_account_spec.rb
+require "./bank_account.rb"
+#spec/bank_account_spec.rb
+
+
+describe BankAccount do
+    
+  it "is created with an opening balance and the name of the client"
+  it "can report it's balance"
+  it "can make deposits"
+  it "can make withdrawals"
+  it "can transfer funds to another bank account"
+  it "has a minimum opening balance"
+  it "allows the bank to change the opening minimum balance"
+end
